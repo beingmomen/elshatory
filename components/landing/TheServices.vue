0651a59e886ccac5636ec18cb66013d32341e7b7
beingmomen <beingmomen@demo.com>
2023-09-29 23:28:26 +0300

    optimize Images

--- components/landing/TheServices.vue
+++ components/landing/TheServices.vue
@@ -9,84 +9,28 @@
     >
       <UCard
         class="cursor-pointer hover:-mt-3 hover:mb-3 transition-all duration-300 ease-in-out"
+        v-for="(service, index) in services"
+        :key="index"
       >
         <div class="h-[12rem] rounded overflow-hidden">
-          <img
-            class="h-full object-cover w-full"
-            src="@/assets/imgs/services/websites.svg"
-            alt="websites"
+          <NuxtImg
+            class="h-full w-full object-cover"
+            :src="service.image"
+            :alt="service.description"
           />
         </div>
 
-        <h3 class="mt-4 font-black text-2xl main-title">Web Development</h3>
+        <h3 class="mt-4 font-black text-2xl main-title">{{ service.title }}</h3>
         <p class="mt-3 text-gray-300">
-          Creating static or dynamic websites for various purposes, such as
-          personal blogs, business websites, e-commerce sites, portfolio
-          websites, and more.
+          {{ service.description }}
         </p>
       </UCard>
-      <UCard
-        class="cursor-pointer hover:-mt-3 hover:mb-3 transition-all duration-300 ease-in-out"
-      >
-        <div class="h-[12rem] rounded overflow-hidden">
-          <img
-            class="h-full object-cover w-full"
-            src="@/assets/imgs/services/node.svg"
-            alt="websites"
-          />
-        </div>
-
-        <h3 class="mt-4 font-black text-2xl main-title">
-          MEVN Stack Developer
-        </h3>
-        <p class="mt-3 text-gray-300">
-          Use MEVN (MongoDB, Express.js, Vue.js, Node.js) for dynamic web apps
-          with real-time features. Build APIs connecting frontend and backend,
-          ensuring modern and efficient user experiences.
-        </p>
-      </UCard>
-      <UCard
-        class="cursor-pointer hover:-mt-3 hover:mb-3 transition-all duration-300 ease-in-out"
-      >
-        <div class="h-[12rem] rounded overflow-hidden">
-          <img
-            class="h-full object-cover w-full"
-            src="@/assets/imgs/services/nuxt-vue.svg"
-            alt="websites"
-          />
-        </div>
-
-        <h3 class="mt-4 font-black text-2xl main-title">
-          Vue.JS, Nuxt.JS Developer
-        </h3>
-        <p class="mt-3 text-gray-300">
-          Vue.js enables interactive web interfaces, while Nuxt.js streamlines
-          server-side rendering for faster, SEO-friendly applications.
-        </p>
-      </UCard>
-      <UCard
-        class="cursor-pointer hover:-mt-3 hover:mb-3 transition-all duration-300 ease-in-out"
-      >
-        <div class="h-[12rem] rounded overflow-hidden">
-          <img
-            class="h-full object-cover w-full"
-            src="@/assets/imgs/services/technical.svg"
-            alt="websites"
-          />
-        </div>
-
-        <h3 class="mt-4 font-black text-2xl main-title">Technical Support</h3>
-        <p class="mt-3 text-gray-300">
-          Support bug fixes, and performance optimization. They ensure
-          cross-browser compatibility, enhance UI, and fix JavaScript issues.
-          They also troubleshoot integrations and boost accessibility.
-        </p></UCard
-      >
     </div>
   </div>
 </template>
 
 <script setup>
+const services = servicesData();
 </script>
 
 <style lang="scss" scoped>
---
+++ composables/servicesData.ts
@@ -0,0 +1,37 @@
+export const servicesData = () => {
+
+  const services = [
+    {
+      id: 1,
+      image: "https://res.cloudinary.com/dyqfclwdk/image/upload/v1696017443/beingmomen/websites_gnakzn.svg",
+      title: "Web Development",
+      description:
+        "Creating static or dynamic websites for various purposes, such as personal blogs, business websites, e-commerce sites, portfolio websites, and more."
+    },
+    {
+      id: 2,
+      image: "https://res.cloudinary.com/dyqfclwdk/image/upload/v1696017442/beingmomen/node_ub9mfe.svg",
+      title: "MEVN Stack Developer",
+      description:
+        "Use MEVN (MongoDB, Express.js, Vue.js, Node.js) for dynamic web apps with real-time features. Build APIs connecting frontend and backend, ensuring modern and efficient user experiences."
+    },
+    {
+      id: 3,
+      image: "https://res.cloudinary.com/dyqfclwdk/image/upload/v1696017448/beingmomen/nuxt-vue_w5aulo.svg",
+      title: "Vue.JS, Nuxt.JS Developer",
+      description:
+        "Vue.js enables interactive web interfaces, while Nuxt.js streamlines server-side rendering for faster, SEO-friendly applications."
+    },
+    {
+      id: 4,
+      image: "https://res.cloudinary.com/dyqfclwdk/image/upload/v1696017452/beingmomen/technical_qjvpr3.svg                                                                               ",
+      title: "Technical Support",
+      description:
+        "Support bug fixes, and performance optimization. They ensure cross-browser compatibility, enhance UI, and fix JavaScript issues. They also troubleshoot integrations and boost accessibility."
+    }
+  ]
+
+
+
+  return services
+}
