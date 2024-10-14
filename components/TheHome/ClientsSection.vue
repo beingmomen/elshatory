41b3050a9845b17267063f4099575401ee8f861c
Maron-X <120732521+Maron-X@users.noreply.github.com>
2024-10-14 19:27:57 +0300

    Remove `dir="ltr"` attribute from ClientsSection component
    The `dir="ltr"` attribute was removed from the root div of the ClientsSection component. This change was made to simplify the component's structure and ensure consistency across different environments where the directionality might be managed by other means.

--- components/TheHome/ClientsSection.vue
+++ components/TheHome/ClientsSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div dir="ltr" id="customers" class="container mx-auto px-8 mt-16">
+  <div id="customers" class="container mx-auto px-8 mt-16">
     <p
       class="font-bold text-xl sm:text-2xl uppercase text-center text-white dark:text-white"
     >
