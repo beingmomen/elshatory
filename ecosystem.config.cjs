404f8e8e7a7f68c212207301d3a71b64efe916f3
beingmomen <abdelmomenelshatory@gmail.com>
2024-12-20 16:15:25 +0200

    Add ecosystem configuration for PM2
    Introduce a new configuration file for PM2 to manage the application with clustering mode and specify the script to run.

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "beingmomen",
+      port: "7777",
+      exec_mode: "cluster",
+      instances: 1,
+      script: "./.output/server/index.mjs",
+    },
+  ],
+};
