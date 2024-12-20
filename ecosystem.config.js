0c75e9aa856afcb23e60d667d2ebb9d62dff6dfb
beingmomen <abdelmomenelshatory@gmail.com>
2024-12-20 15:19:13 +0200

    Add port configuration to ecosystem.config.js
    Added a port configuration for the "beingmomen" app in the ecosystem.config.js file to specify the port number as 3579. This change ensures that the application runs on the designated port, which is essential for proper deployment and management.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps: [
     {
       name: "beingmomen",
+      port: 3579,
       exec_mode: "cluster",
       instances: "max", // Or a number of instances
       script: "./node_modules/nuxt/bin/nuxt.js",
