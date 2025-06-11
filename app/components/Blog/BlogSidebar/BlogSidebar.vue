d53620d0bf4cb6915f96090e25ece0f9c27a4d4f
beingmomen <abdelmomenelshatory@gmail.com>
2025-06-11 18:55:14 +0300

    refactor(blog): extract table-of-contents styles to separate file
    Move table-of-contents styles from BlogSidebar component to dedicated SCSS file for better maintainability and reuse. Also refactor social networks array into a script variable.

--- app/components/Blog/BlogSidebar/BlogSidebar.vue
+++ app/components/Blog/BlogSidebar/BlogSidebar.vue
@@ -4,17 +4,9 @@
     <USeparator class="my-5" size="sm" />
     <BlogBlogSidebarLinks />
     <USeparator class="my-5" size="sm" type="dashed" />
-    <!-- <CommonCommonSocialPart /> -->
     <div class="flex justify-between">
       <SocialShare
-        v-for="network in [
-          'facebook',
-          'x',
-          'linkedin',
-          'reddit',
-          'whatsapp',
-          'telegram',
-        ]"
+        v-for="network in networks"
         :key="network"
         :network="network"
         :label="false"
@@ -23,57 +15,17 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const networks = [
+  "facebook",
+  "x",
+  "linkedin",
+  "reddit",
+  "whatsapp",
+  "telegram",
+];
+</script>
 
 <style lang="scss" scoped>
-.table-of-contents {
-  position: sticky;
-  top: 2rem;
-  max-height: calc(100vh - 4rem);
-  overflow-y: auto;
-  z-index: 100;
-  direction: rtl;
-}
-
-.toc-navigation {
-  direction: rtl;
-}
-
-// Custom scrollbar for the sticky TOC
-.table-of-contents {
-  scrollbar-width: thin;
-  scrollbar-color: rgb(209 213 219) transparent;
-
-  &::-webkit-scrollbar {
-    width: 4px;
-  }
-
-  &::-webkit-scrollbar-track {
-    background: transparent;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background-color: rgb(209 213 219);
-    border-radius: 2px;
-  }
-
-  &::-webkit-scrollbar-thumb:hover {
-    background-color: rgb(156 163 175);
-  }
-}
-
-// Additional styling for level indicators
-.toc-level-2 {
-  font-weight: 600;
-}
-
-.toc-level-3 {
-  font-weight: 400;
-  opacity: 0.8;
-}
-
-.toc-level-4 {
-  font-weight: 400;
-  opacity: 0.7;
-}
+@use "assets/scss/table-of-contents";
 </style>
