5d9c435c08bf565c809600ffc7bb74672ce164d9
beingmomen <abdelmomenelshatory@gmail.com>
2025-06-12 14:21:20 +0300

    build: add dotenv for environment configuration
    - Add dotenv package to manage environment variables
    - Update ecosystem.config.cjs to use PORT from environment variables

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,13 @@
+require("dotenv").config();
+
 module.exports = {
   apps: [
     {
       name: "beingmomen",
-      script: "./.output/server/index.mjs",
+      port: process.env.PORT,
       exec_mode: "cluster",
       instances: "max",
-      env: {
-        PORT: 9997,
-      },
+      script: "./.output/server/index.mjs",
     },
   ],
 };
